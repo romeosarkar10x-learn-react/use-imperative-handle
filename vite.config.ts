91c690301a51f1c6f9d5c5c06b648329d6b462d9
Romeo Sarkar <romeosarkar10x@gmail.com>
2026-05-02 16:40:56 +0530

    Import tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from "node:path";
 import { defineConfig } from "vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite";
 
 function getBaseURL(): string {
     const githubRepository = process.env.GITHUB_REPOSITORY;
